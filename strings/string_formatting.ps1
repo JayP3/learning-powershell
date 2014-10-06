aa359252629b66776c0052852301193d837e1ed6
Jason Pierrepont <json6381@aol.com>
2014-10-06 19:01:44 -0400

    Initial Commit. String Formatting

---
+++ strings/string_formatting.ps1
@@ -0,0 +1,23 @@
+﻿# Create a few variables
+$state = "NY"
+$city = "Maspeth"
+Clear-Host
+# We can print these to the screen like this
+Write-Host "`r`n****Just writing a couple variables to the screen.****"
+Write-Host $state 
+Write-Host $city
+
+# Now to write a sentence with both variables in it
+# they can just be included in the string like this
+# 1. String Expansion
+Write-Host "`r`n****Using String Expansion****"
+Write-Host "I live in the state of $state, in the city $city."
+
+# Other ways to format strings
+# 2. .NET Format method. System.String.Format
+Write-Host "`r`n****Using the .NET String Format Method****"
+[string]::Format("I live in the state of {0}, in the city {1}.",$state, $city)
+
+# 3. Powershell formatting. -f
+Write-Host "`r`n****Using Powershell String Formatting****"
+"I live in the state of {0}, in the city {1}." -f $state, $city
