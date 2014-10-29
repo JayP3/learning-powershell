91d10e28e506dfd948a1f614be8011e0f2663283
Jason Pierrepont <json6381@aol.com>
2014-10-29 18:54:29 -0400

    Added file with links to tutorials. Added reading files script

---
+++ files/reading_files01.ps1
@@ -0,0 +1,13 @@
+﻿# Get-Content
+# Aliases cat, type, gc
+# Reads a file one line at a time.
+
+Get-Content $args[0]
+
+# Get-Content produces an array of strings. One string for each line in the file
+
+# You can also pipe Get-Content into Foreach or Foreach-Object
+# to process a file one line at a time.
+Get-Content $args[0] | ForEach-Object -Process {
+    Write-Host $_
+}
---
+++ learning_resources.md
@@ -0,0 +1,4 @@
+<http://ss64.com/ps/>
+
+<http://technet.microsoft.com/en-us/library/hh848793.aspx>
+
